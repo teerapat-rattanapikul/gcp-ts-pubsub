15deda744ce82d4f1c98a36cf2263b0111195a7b
teerapat-rattanapikul <teerapat@digio.co.th>
2022-10-31 15:26:50 +0700

    0.1

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-const helloPubSub = (event: any, context: any) => {
+export const helloPubSub = (event: any, context: any) => {
   const message = event.data
     ? event.data : 'Hello, World';
   console.log(message);
   console.log('...running')
 };
-
-export = helloPubSub
 
 
