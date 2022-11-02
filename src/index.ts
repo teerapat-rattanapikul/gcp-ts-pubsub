7ebe8f4e266602ac788ecaa7c8d0cac39bda52d7
teerapat-rattanapikul <teerapat@digio.co.th>
2022-11-02 17:09:29 +0700

    rollback

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-import { NestFactory } from '@nestjs/core'
-import { ExpressAdapter } from '@nestjs/platform-express'
-import express from 'express'
-
-
 export const helloPubSub = (event: any, context: any) => {
-    const message = event.data || null
+  const message = event.data
+    ? event.data : 'Hello, World';
+  console.log(message);
+  console.log('...running')
+};
 
-    const server = express()
 
-    class AppModule {}
-
-    console.log(message)
-
-    const createNestServer = async (expressInstance) => {
-        const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance))
-        return app.init()
-    }
-
-    createNestServer(server)
-    .then((v) => {
-        console.log(message)
-        console.log('running....')
-    })
-}
