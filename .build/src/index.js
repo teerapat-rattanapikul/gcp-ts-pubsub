aa1c101b401e387ea011a3622a9da6dc51985123
teerapat-rattanapikul <teerapat@digio.co.th>
2022-11-03 14:45:53 +0700

    build 0.22

--- .build/src/index.js
+++ .build/src/index.js
@@ -2,18 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
 const core_1 = require("@nestjs/core");
-const createNestServer = async () => {
-    const moduless = class BatchModule {
+const helloWorld = (event, context) => {
+    const createNestServer = async () => {
+        const moduless = class BatchModule {
+        };
+        const app = await core_1.NestFactory.create(moduless);
+        return app.init();
     };
-    const app = await core_1.NestFactory.create(moduless);
-    return app.init();
-};
-const helloWorld = async (event, context) => {
-    await createNestServer();
-    const message = event.data
-        ? event.data : 'Hello, World';
-    console.log(message);
-    console.log('...running');
+    createNestServer()
+        .then((v) => {
+        console.log('running....');
+    });
 };
 exports.helloWorld = helloWorld;
 //# sourceMappingURL=index.js.map
