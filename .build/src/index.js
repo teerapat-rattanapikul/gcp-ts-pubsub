a347c588f195b605d1314dc49276b8c3ebaaffd1
teerapat-rattanapikul <teerapat@digio.co.th>
2022-11-03 15:25:20 +0700

    445

--- .build/src/index.js
+++ .build/src/index.js
@@ -1,11 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
-const core_1 = require("@nestjs/core");
 const helloWorld = (event, context) => {
     const createNestServer = async () => {
-        const app = await core_1.NestFactory.create({});
-        return app.init();
     };
     console.log('running....');
 };
