e90f9f3a9d26d7d58effc3c6de90ef44082e98a7
teerapat-rattanapikul <teerapat@digio.co.th>
2022-11-03 14:44:17 +0700

    fix0.22

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 
-const createNestServer = async () => {
-  const moduless = class BatchModule {}
-  const app = await NestFactory.create(moduless)
-  return app.init()
-}
+export const helloWorld = (event: any, context: any) => {
 
-export const helloWorld = async (event: any, context: any) => {
-
-  await createNestServer()
+  const createNestServer = async () => {
+    const moduless = class BatchModule {}
+    const app = await NestFactory.create(moduless)
+    return app.init()
+  }
   
-  const message = event.data
-    ? event.data : 'Hello, World';
-  console.log(message);
-  console.log('...running')
+  createNestServer()
+  .then((v) => {
+    console.log('running....')
+  })
 };
