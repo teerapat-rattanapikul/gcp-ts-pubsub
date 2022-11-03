c0e81f7008e236f479ce17f5cef3b477a36b3011
teerapat-rattanapikul <teerapat@digio.co.th>
2022-11-03 14:49:34 +0700

    fix build

--- .build/src/index.js
+++ .build/src/index.js
@@ -1,18 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
-const core_1 = require("@nestjs/core");
-const helloWorld = (event, context) => {
-    const createNestServer = async () => {
-        const moduless = class BatchModule {
-        };
-        const app = await core_1.NestFactory.create(moduless);
-        return app.init();
-    };
-    createNestServer()
-        .then((v) => {
-        console.log('running....');
-    });
+var nestt = require('@nestjs/core');
+exports.helloPubSub = (event, context) => {
+  const message = event.data || null
+
+  const createNestServer = async () => {
+    const moduless = class BatchModule {}
+    const app = await nestt.NestFactory.create(moduless)
+    return app.init()
+  }
+
+  createNestServer()
+  .then((v) => {
+    console.log(message)
+    console.log('running....')
+  })
 };
-exports.helloWorld = helloWorld;
+
 //# sourceMappingURL=index.js.map
