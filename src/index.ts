07e504b1f24f09adf9fa71c32de9e8d00f7e426f
teerapat-rattanapikul <teerapat@digio.co.th>
2022-11-03 14:02:17 +0700

    add import

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { NestFactory } from '@nestjs/core'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import express from 'express'
+
 export const helloWorld = (event: any, context: any) => {
   const message = event.data
     ? event.data : 'Hello, World';
