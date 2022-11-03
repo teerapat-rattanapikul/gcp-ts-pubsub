60c90fd09cd1d54aa5bf17dca3d59ade24bf9cbe
teerapat-rattanapikul <teerapat@digio.co.th>
2022-11-03 15:19:57 +0700

    let it fail build

--- .build/src/index.js
+++ .build/src/index.js
@@ -4,9 +4,7 @@ exports.helloWorld = void 0;
 const core_1 = require("@nestjs/core");
 const helloWorld = (event, context) => {
     const createNestServer = async () => {
-        const moduless = class BatchModule {
-        };
-        const app = await core_1.NestFactory.create(moduless);
+        const app = await core_1.NestFactory.create({});
         return app.init();
     };
     console.log('running....');
