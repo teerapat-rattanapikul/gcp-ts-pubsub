9a8d3a8fcb5664dc2288686886ff1c1a2f570ff0
teerapat-rattanapikul <teerapat@digio.co.th>
2022-11-14 15:58:09 +0700

    init__v2

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,7 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
-function helloWorld(event, context) {
+const core_1 = require("@nestjs/core");
+async function helloWorld(event, context) {
+    const createNestServer = async () => {
+        const moduless = class BatchModule {
+        };
+        const app = await core_1.NestFactory.create(moduless);
+        return app.init();
+    };
+    await createNestServer();
     console.log('running....');
 }
 exports.helloWorld = helloWorld;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-export function helloWorld (event: any, context: any) {
+import {  NestFactory } from '@nestjs/core'
 
+export async function helloWorld (event: any, context: any):Promise<any> {
+
+    const createNestServer = async () => {
+      const moduless = class BatchModule {}
+      const app = await NestFactory.create(moduless)
+      return app.init()
+    }
+
+    await createNestServer()
     console.log('running....')
   };
