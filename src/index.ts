126661b9d7e112fc2bbc7355b30e9d81941ea367
teerapat-rattanapikul <teerapat@digio.co.th>
2022-11-17 15:04:28 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import {  NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 export async function helloWorld (event: any, context: any):Promise<any> {
 
     const createNestServer = async () => {
       const moduless = class BatchModule {}
-      const app = await NestFactory.create(moduless)
+      const app = await NestFactory.create<NestExpressApplication>(moduless)
       return app.init()
     }
 
